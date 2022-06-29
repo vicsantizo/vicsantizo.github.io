ed3f7b9341b46d856625bd775715f1ca79ce38b4
vicsantizo <santizo.vic@gmail.com>
2022-06-28 20:36:52 -0600

    Add config for loading assets

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js', 
         clean: true,
+        assetModuleFilename: '[name][ext]',
     },
     devtool: 'source-map',
     devServer: {
@@ -45,9 +46,10 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
-
-                    
-                
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i, 
+                type: 'asset/resource'
             }
         ],
         
